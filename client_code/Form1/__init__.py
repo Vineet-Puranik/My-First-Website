5cd465a3abcbfb3ac81d7a6be13492933630f527
vineetpuranik@gmail.com <vineetpuranik@gmail.com>
2024-03-20 20:11:04 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -5,5 +5,14 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.bannerLbl.text = 'My Webpage Banner'
 
     # Any code you write here will run before the form opens.
+  def AddABtn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.bannerLbl.text += 'a'
+
+  def DELletter_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.bannerLbl.text = self.bannerLbl.text[:-1]
+
