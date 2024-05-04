2a6eccad634796e5bee24730fcdfde01da3fc1b4
vineetpuranik@gmail.com <vineetpuranik@gmail.com>
2024-05-04 06:41:54 +0000

    Created Form1.ItemTemplate1; Edited Form1

---
+++ client_code/Form1/ItemTemplate1/__init__.py
@@ -0,0 +1,31 @@
+from ._anvil_designer import ItemTemplate1Template
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class ItemTemplate1(ItemTemplate1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+self.label_1.text = self.item['Name'] + ':' +str(self.item['When'])
+    self.label_2.text = '---------------------------------------------------------------------'
+    # Any code you write here will run before the form opens.
+
+from ._anvil_designer import ItemTemplate1Template
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class ItemTemplate1(ItemTemplate1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+    self.label_1.text = self.item['Name'] + ':' +str(self.item['When'])
+    self.label_2.text = '---------------------------------------------------------------------'
